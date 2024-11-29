37cefdbec2ae286231b45624cc61850504dc1ba9
curso07 <alejandro.cald@gmail.com>
2024-11-29 13:36:50 +0100

    Armado de Fileprocessor y primer Commit.

---
+++ src/main/java/es/santander/ascender/proyecto11/FileProcesor.java
@@ -0,0 +1,28 @@
+package es.santander.ascender.proyecto11;
+
+public class FileProcesor implements IFileProcesor{
+
+    @Override
+    public String leerFile(String filePath) throws Exception {
+
+        new UnsupportedOperationException();
+
+        return "";
+    };
+
+    @Override
+        public String eliminarVocales(String input) {
+    
+            new UnsupportedOperationException();
+    
+            return "";
+        };
+
+    @Override
+    public void escribirAFile(String filePath, String content) throws Exception {
+
+        new UnsupportedOperationException();
+
+    };
+}
+
